8fc477d52e544b610619a85627e0d7f6d7894066
leeg <leeg@d26fb610-7416-4088-9ce3-2b22d1cb08f6>
2020-03-11 16:53:48 +0000

    Test for releasing a pointer
    git-svn-id: svn://stevenrbaker.com/Amiga-Smalltalk/trunk@41 d26fb610-7416-4088-9ce3-2b22d1cb08f6

--- AllocationTests.c
+++ AllocationTests.c
@@ -1,5 +1,19 @@
 #include "AllocationTests.h"
+#include "ObjectMemory_Allocation.h"
+#include "ObjectMemory_FreeList.h"
+#include "ObjectMemory_Storage.h"
+
+Test(ReleasePointer) {
+  ObjectPointer objectPointer = 0x3580, freeHead = 0x8530;
+  ObjectMemory_headOfFreePointerList_put(freeHead);
+  ObjectMemory_freeBitOf_put(objectPointer, 0);
+
+  ObjectMemory_releasePointer(objectPointer);
+
+  Expect(ObjectMemory_freeBitOf(objectPointer) == 1);
+  Expect(ObjectMemory_headOfFreePointerList() == objectPointer);
+}
 
 void AllocationTests(struct TestResult *tr) {
-  
+  RunTest(ReleasePointer);
 }
