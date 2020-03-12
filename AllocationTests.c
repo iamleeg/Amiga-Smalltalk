be650cbb37554979bfa6b0b3c23fdf1c1a402ada
leeg <leeg@d26fb610-7416-4088-9ce3-2b22d1cb08f6>
2020-03-12 16:23:59 +0000

    Positive test of pointer reversal
    git-svn-id: svn://stevenrbaker.com/Amiga-Smalltalk/trunk@43 d26fb610-7416-4088-9ce3-2b22d1cb08f6

--- AllocationTests.c
+++ AllocationTests.c
@@ -30,7 +30,26 @@ Test(LowWaterMarkCalculation) {
   Expect(ObjectMemory_freeBitOf(freeRegion) == 1);
 }
 
+Test(ReverseHeapPointerAboveLowWaterMark) {
+  Word segment = 2, size = 0x0a, retrievedSize = 0x0;
+  ObjectPointer lowWaterMark = 0x1000,
+    location = 0x2000,
+    objectPointer = 0x1010;
+  currentSegment = segment;
+  ObjectMemory_segmentBitsOf_put(objectPointer, segment);
+  ObjectMemory_freeBitOf_put(objectPointer, 0);
+  ObjectMemory_locationBitsOf_put(objectPointer, location);
+  ObjectMemory_sizeBitsOf_put(objectPointer, size);
+
+  ObjectMemory_reverseHeapPointersAbove(lowWaterMark);
+
+  retrievedSize = RealWordMemory_segment_word(segment, location);
+  Expect(ObjectMemory_locationBitsOf(objectPointer) == size);
+  Expect(retrievedSize == objectPointer);
+}
+
 void AllocationTests(struct TestResult *tr) {
   RunTest(ReleasePointer);
   RunTest(LowWaterMarkCalculation);
+  RunTest(ReverseHeapPointerAboveLowWaterMark);
 }
