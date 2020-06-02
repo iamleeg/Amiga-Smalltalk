377eb9c1dcbedc61cced69866897ba4ac68171da
Graham Lee <graham.lee@cs.ox.ac.uk>
2020-06-02 17:31:30 +0100

    Another condition to test method lookups

--- ClassTests.c
+++ ClassTests.c
@@ -64,6 +64,7 @@ Test(FindingMethodInDictionaryWithWrongKeyFails) {
   ObjectMemory_segmentBitsOf_put(array, segment);
   ObjectMemory_sizeBitsOf_put(array, HeaderSize + 1);
   ObjectMemory_storeWord_ofObject_withValue(0, array, compiledMethod);
+  ObjectMemory_storeWord_ofObject_withValue(MethodArrayIndex, dictionary, array);
 
   messageSelector = symbol + 2;
 
@@ -96,10 +97,36 @@ Test(FindingMethodInDictionaryWithCorrectKeySucceeds) {
   Expect(newMethod == compiledMethod);
 }
 
+Test(FindingMethodInDictionaryWithMultipleWrongKeysFails) {
+  ObjectPointer dictionary = 0x2600, location = 0x3006, array = 0x2610, arrayLocation = 0x3016, compiledMethod = 0xf002, symbol = DoesNotUnderstandSelector, otherSymbol = 0x1030, otherMethod = 0xf008;
+  Word dictionarySize = SelectorStart + HeaderSize + 1, segment = RealWordMemory_bestSegmentFor(3);
+  Bool foundMethod;
+
+  ObjectMemory_locationBitsOf_put(dictionary, location);
+  ObjectMemory_segmentBitsOf_put(dictionary, segment);
+  ObjectMemory_sizeBitsOf_put(dictionary, dictionarySize);
+  ObjectMemory_storeWord_ofObject_withValue(SelectorStart, dictionary, symbol);
+  ObjectMemory_storeWord_ofObject_withValue(SelectorStart + 1, dictionary, otherSymbol);
+
+  ObjectMemory_locationBitsOf_put(array, arrayLocation);
+  ObjectMemory_segmentBitsOf_put(array, segment);
+  ObjectMemory_sizeBitsOf_put(array, HeaderSize + 2);
+  ObjectMemory_storeWord_ofObject_withValue(0, array, compiledMethod);
+  ObjectMemory_storeWord_ofObject_withValue(1, array, otherMethod);
+  ObjectMemory_storeWord_ofObject_withValue(MethodArrayIndex, dictionary, array);
+
+  messageSelector = symbol + 2;
+
+  foundMethod = Interpreter_lookupMethodInDictionary(dictionary);
+
+  Expect(foundMethod == NO);
+}
+
 void ClassTests(struct TestResult *tr) {
   RunTest(TestHashOfDifferentObjectsIsDifferent);
   RunTest(FindingMethodInEmptyDictionaryFails);
   RunTest(FindingMethodInDictionaryWithNilKeyFails);
   RunTest(FindingMethodInDictionaryWithWrongKeyFails);
   RunTest(FindingMethodInDictionaryWithCorrectKeySucceeds);
+  RunTest(FindingMethodInDictionaryWithMultipleWrongKeysFails);
 }
