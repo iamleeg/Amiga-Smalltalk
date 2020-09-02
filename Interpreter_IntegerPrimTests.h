8b0abbe19c6ff7eac244dd1abdf32fb3c5f1b09e
Alan Francis <acf@alancfrancis.com>
2020-09-02 21:24:07 +0100

    Break up the tests and add tests for the bitwise primitives and makepoint

---
+++ Interpreter_IntegerPrimTests.h
@@ -0,0 +1,8 @@
+#ifndef AMIGA_SMALLTALK_INTERPRETERINTEGERPRIM_TESTS_H
+#define AMIGA_SMALLTALK_INTERPRETERINTEGERPRIM_TESTS_H
+
+#include "tests.h"
+
+void InterpreterIntegerPrimitiveTests(struct TestResult *tr);
+
+#endif
--- Interpreter_PrimArithTests.h
+++
@@ -1,8 +0,0 @@
-#ifndef AMIGA_SMALLTALK_INTERPRETERPRIMARITH_TESTS_H
-#define AMIGA_SMALLTALK_INTERPRETERPRIMARITH_TESTS_H
-
-#include "tests.h"
-
-void InterpreterArithmeticPrimitiveTests(struct TestResult *tr);
-
-#endif
