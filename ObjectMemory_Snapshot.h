29c65ad1075db4c8ca751378aa96b0971e1c6d5e
Alan Francis <acf@alancfrancis.com>
2020-12-12 14:32:06 +0000

    Load and resave the original smalltalk80 image file downloaded from http://www.wolczko.com/st80/ and renamed from VirtualImage to image.st

--- ObjectMemory_Snapshot.h
+++ ObjectMemory_Snapshot.h
@@ -1,6 +1,7 @@
 #include <proto/dos.h>
 #include <dos/dos.h>
+#include <exec/types.h>
 
-BOOL write_placeholder(BPTR filehandle);
-BOOL write_interchange(BPTR filehandle);
-BOOL save_snapshot(STRPTR name);
+BOOL save_snapshot(CONST_STRPTR name);
+BOOL load_snapshot(CONST_STRPTR name);
+
